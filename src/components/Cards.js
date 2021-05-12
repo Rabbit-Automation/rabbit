cd5f6640879b512307bb4796b69a1106bd34f0b2
Josue_Reyes <black_cat_train@hotmail.com>
2021-05-11 19:11:20 -0500

    Se añadieron nuevas secciones y cambios de estilos al navbar (reparar estilos)

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,74 +1,141 @@
-import React from 'react'
-import CardItem from './CardItem'
-import './Cards.css'
+import React from "react";
+import CardItem from "./CardItem";
+import "./Cards.css";
 
 function Cards() {
-    return (
-        <div className='cards' id='conocenos'>
-            <h2>Con&oacute;cenos</h2>            
-						<div className="container">
-							<div className="row">
-								<div className="col-12">
-									<div className="card-body text-center">
-										<p>Medimos el desempeño de cualquier proceso industrial con el fin de mejorar su eficiencia, rentabilidad y productividad desarrollando sistemas a la medida para sus sistemas de <strong>Smart Factory</strong></p>
-									</div>
-								</div>
-							</div>
-						</div>
-						<div class="card-deck">
-								<div class="card">
-									<div className="card__icon">
-										<i class="fas fa-database"></i>
-									</div>
-									<div className="card-body text-center">
-										<strong className="card-title">Base de datos</strong>
-									</div>
-									<div className="card-footer card__description">
-										<p>Last updated 3 mins ago</p>
-									</div>
-								</div>
-								<div className="card">
-									<div className="card__icon">
-										<i className="fas fa-wifi"></i>
-									</div>
-									<div className="card-body text-center">
-										<strong className="card-title">Conectividad</strong>
-									</div>
-								</div>
-								<div className="card">
-									<div className="card__icon">
-										<i className="fas fa-desktop"></i>
-									</div>
-									<div className="card-body text-center">
-										<strong className="card-title">Plataforma de monitoreo</strong>
-									</div>
-								</div>
-								<div className="card">
-									<div className="card__icon">
-										<i className="fas fa-file-medical-alt"></i>
-									</div>
-									<div className="card-body text-center">
-										<strong className="card-title">Reportes en tiempo real</strong>
-									</div>
-								</div>
-								<div className="card">
-									<div className="card__icon">
-										<i className="fas fa-file-medical-alt"></i>
-									</div>
-									<div className="card-body text-center">
-										<strong className="card-title">Reportes en tiempo real</strong>
-									</div>
-								</div>
-						</div>
-            <div className="cards__container">
-                <div className="cards__wrapper">
-                    <ul className="cards__item">
-                        <CardItem />
-                    </ul>
-                </div>
+  return (
+    <div className="cards" id="conocenos">
+      <h2>Con&oacute;cenos</h2>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card-body text-center">
+              <p>
+                Medimos el desempeño de cualquier proceso industrial con el fin
+                de mejorar su eficiencia, rentabilidad y productividad
+                desarrollando sistemas a la medida para sus sistemas de
+                <strong>Smart Factory</strong>
+              </p>
             </div>
+          </div>
         </div>
-    )
+      </div>
+      <div className="card-deck">
+        <div className="card">
+          <div className="card__icon">
+            <i className="fas fa-database"></i>
+          </div>
+          <div className="card-body text-center">
+            <strong className="card-title">Base de datos</strong>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__icon">
+            <i className="fas fa-wifi"></i>
+          </div>
+          <div className="card-body text-center">
+            <strong className="card-title">Conectividad</strong>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__icon">
+            <i className="fas fa-desktop"></i>
+          </div>
+          <div className="card-body text-center">
+            <strong className="card-title">Plataforma de monitoreo</strong>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card__icon">
+            <i className="fas fa-file-medical-alt"></i>
+          </div>
+          <div className="card-body text-center">
+            <strong className="card-title">Reportes en tiempo real</strong>
+          </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card-header text-center">
+              <h3>Beneficios</h3>
+            </div>
+            <div className="card-deck">
+              <div className="card">
+                <div className="card-body">
+                  <ul>
+                    <li>
+                      <p>Mejora de la productividad.</p>
+                    </li>
+                    <li>
+                      <p>Prevención oportuna de perdidas.</p>
+                    </li>
+                    <li>
+                      <p>
+                        Toma inmediata de acciones y aumentar la velocidad de
+                        los procesos.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  src="/images/general/beneficios.png"
+                  width="400"
+                  alt="beneficios del monitoreo"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="card-header text-center">
+              <h3>Marcas</h3>
+            </div>
+            <div className="card-deck">
+              <div className="card">
+                <div className="card-body">
+                  <ul>
+                    <li>
+                      <p>Mejora de la productividad.</p>
+                    </li>
+                    <li>
+                      <p>Prevención oportuna de perdidas.</p>
+                    </li>
+                    <li>
+                      <p>
+                        Toma inmediata de acciones y aumentar la velocidad de
+                        los procesos.
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card">
+                <img
+                  src="/images/general/beneficios.png"
+                  width="400"
+                  alt="fsda"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <ul className="cards__item">
+            <CardItem />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Cards;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Button } from './Button';
-import './Navbar.css';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
+import "./Navbar.css";
 
 function Navbar() {
   const [click, setClick] = useState(false);
   const [button, setButton] = useState(true);
 
-  const handleClick = () => setClick(!click); 
+  const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
 
   const showButton = () => {
-    if(window.innerWidth <= 1100){
+    if (window.innerWidth <= 1100) {
       setButton(false);
     } else {
       setButton(true);
@@ -19,47 +19,59 @@ function Navbar() {
   };
 
   useEffect(() => {
-     showButton();
-  }, [])
-  window.addEventListener('resize', showButton);
+    showButton();
+  }, []);
+  window.addEventListener("resize", showButton);
 
   return (
     <>
-    <nav className='navbar'>
-      <div className='navbar-container'>
-        <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-          <img src='/images/general/logo_b.png' alt='Rabbit Automation and Technologies logo' width='70'/>
-        </Link>
-        <div className='menu-icon' onClick={handleClick}>
-          <i className={ click ? 'fas fa-times' : 'fas fa-bars'}/>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+            <img
+              src="/images/general/logo_b.png"
+              alt="Rabbit Automation and Technologies logo"
+              width="100"
+            />
+          </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Inicio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="#venta_productos"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Marcas
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/demos" className="nav-links" onClick={closeMobileMenu}>
+                Soluciones
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                to="/contacto"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Contacto
+              </Link>
+            </li>
+          </ul>
+          {/* {button && <Button buttonStyle='btn--outline'>SOLUCIONES</Button>} */}
         </div>
-        <ul className={ click ? 'nav-menu active' : 'nav-menu' }>
-          <li className="nav-item">
-            <Link to='/' className='nav-links' onClick={ closeMobileMenu }>
-              RABBIT
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to='#venta_productos' className='nav-links' onClick={ closeMobileMenu }>
-              VENTA DE MATERIAL
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/demos' className='nav-links' onClick={ closeMobileMenu }>
-              SOLUCIONES
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to='/contacto' className='nav-links' onClick={ closeMobileMenu }>
-              CONTACTO
-            </Link>
-          </li>
-        </ul>
-        {/* {button && <Button buttonStyle='btn--outline'>SOLUCIONES</Button>} */}
-      </div>
-    </nav>
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar;
