d8954f5bce599b91b36ef2803b2b407946188b56
Josue_Reyes <black_cat_train@hotmail.com>
2021-05-18 09:56:51 -0500

    Updates

---
+++ precache-manifest.efea09376045385660ba87eb57cba5e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "15e8ea0b6a537e0d834c",
+    "url": "/static/css/main.9fb0e18a.chunk.css"
+  },
+  {
+    "revision": "15e8ea0b6a537e0d834c",
+    "url": "/static/js/main.1a7c19be.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d723156fc4ac25d616dd",
+    "url": "/static/js/2.26e7cd8c.chunk.js"
+  },
+  {
+    "revision": "1d9b905bb7c566c1e37dfa027d8d0f4f",
+    "url": "/index.html"
+  }
+];
